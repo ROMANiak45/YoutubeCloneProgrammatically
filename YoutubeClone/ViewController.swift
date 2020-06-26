f0b2398588c52e816087e12d952d4cf63b9077c6
Roman Croitor <croitorr@gmail.com>
2020-06-26 16:09:02 +0300

    Initial Commit

---
+++ YoutubeClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoutubeClone
+//
+//  Created by Roman Croitor on 26/06/2020.
+//  Copyright © 2020 Roman Croitor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
